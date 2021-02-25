22bd978f93ca4cf69ad6e6abac1524fe1e8c67a0
Haoran He <hrgutou@gmail.com>
2021-02-25 00:09:52 -0500

    Fixed HTTPS method

--- src/main/java/com/instagclone/imageservice/ImageServiceApplication.java
+++ src/main/java/com/instagclone/imageservice/ImageServiceApplication.java
@@ -1,12 +1,9 @@
 package com.instagclone.imageservice;
 
-import com.instagclone.imageservice.controller.ImageServiceAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.instagclone"})
 public class ImageServiceApplication {
 
     public static void main(String[] args) {
